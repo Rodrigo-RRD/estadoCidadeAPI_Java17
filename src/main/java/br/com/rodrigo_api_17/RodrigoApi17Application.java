d53074fa93f048ee4a8b5f29e77a1fc2e5d415b5
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-08-14 20:11:27 -0300

    Add projeto

---
+++ src/main/java/br/com/rodrigo_api_17/RodrigoApi17Application.java
@@ -0,0 +1,13 @@
+package br.com.rodrigo_api_17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodrigoApi17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodrigoApi17Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigo_api_17/RodrigoApi17ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigo_api_17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodrigoApi17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
